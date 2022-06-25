ca7f41e187debb7fe2457084087d09fdd6367f8d
Harsh <valambeharsh1701@gmail.com>
2022-06-25 21:35:07 +0530

    complete

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -3,7 +3,7 @@ import { CloudDownloadIcon,DatabaseIcon,PaperAirplaneIcon,ServerIcon } from '@he
 import bgImg from '../assets/cyber-bg.png'
 const Main = () => {
   return (
-    <div name='home' className='w-full h-screen bg-zinc-200 flex flex-col justify-between'>
+    <div name='home' className='w-full h-screen bg-zinc-200 pt-6 flex flex-col justify-between'>
         <div className='grid md:grid-cols-2 max-w-[1240px] m-auto'>
             <div className='flex flex-col justify-center md:items-start w-full px-2 py-8'>
                 <p className='text-2xl'>Unique Sequencing & Production</p>
@@ -14,10 +14,11 @@ const Main = () => {
             <div>
                 <img className='w-full' src={bgImg} alt="/" />
             </div>
-            <div className='absolute flex flex-col py-8 md:min-w-[760px] bottom-[5%]
+            <div className=' flex flex-col justify-center py-8 md:min-w-[760px] bottom-[5%]
             mx-1 md:left-1/2 transform md:-translate-x-1/2 bg-zinc-200
             border border-slate-300 rounded-xl text-center shadow-xl'>
                 <p>Data Services</p>
+                
                 <div className='flex justify-between flex-wrap px-4'>
                     <p className='flex px-4 py-2 text-slate-500'><CloudDownloadIcon className='h-6 text-indigo-600' /> App Security</p>
                     <p className='flex px-4 py-2 text-slate-500'><DatabaseIcon className='h-6 text-indigo-600' /> Dashboard Design</p>
